65c9dca13e2a63dc43776eb39025025e83054c9b
Takuya Akiyama <ta-akiyama@TakuyanoMacBook-Pro-4.local>
2018-04-20 17:50:39 +0900

    admin画面を作成

--- submit/admin.py
+++ submit/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from submit.models import SubmitModel
+from submit.models import SubmitModel, ScoreModel
 
 class SubmitAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'filename', 'submit_timestamp', 'score_date_start', 'score_date_end', 'score_avg')
-    #list_display_links = ('id')
+    list_display = ('id', 'username', 'filename', 'submit_timestamp',
+                    'score_date_start', 'score_date_end', 'score_avg', 'for_score_simulation')
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'submit', 'score_date', 'score')
 
 admin.site.register(SubmitModel, SubmitAdmin)
+admin.site.register(ScoreModel, ScoreAdmin)
 
