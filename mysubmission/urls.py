f94931bb6b4c31171a02c35da911d551be34084e
ruik <kimura.rui@gmail.com>
2018-04-26 16:10:03 +0900

    stationごとのグラフを追加

--- mysubmission/urls.py
+++ mysubmission/urls.py
@@ -5,4 +5,5 @@ app_name = 'mysubmission'
 urlpatterns = [
     path('', views.mysubmission_base_view, name='mysubmission_base_view'),
     path('detail', views.mysubmission_detail_view, name='mysubmission_detail_view'),
+    path('detail_chart', views.mysubmission_detail_chart_view, name='mysubmission_detail_chart_view'),
 ]
--- mysubmission/views.py
+++ mysubmission/views.py
@@ -101,3 +101,77 @@ def mysubmission_detail_view(request):
     }
 
     return render(request, 'mysubmission/mysubmission_detail.html', context)
+
+def mysubmission_detail_chart_view(request):
+    if request.method == 'POST':
+        submit_id = request.POST['submit_id']
+        city = request.POST['city']
+        target = request.POST['target']
+        score_date = request.POST['score_date']
+    elif request.method == 'GET':
+        submit_id = request.GET['submit_id']
+        city = request.GET['city']
+        target = request.GET['target']
+        score_date = request.GET['score_date']
+    scores = ScoreModel.objects.filter(submit=submit_id)
+
+    # for chart
+    submit = SubmitModel.objects.filter(id=submit_id)[0]
+
+    # label data
+    dir_name = city
+    label_data = pd.DataFrame(index=[], columns=['station_id','record_time','PM2.5','PM10','NO2','CO','O3','SO2'])
+    for p in pd.date_range(pd.to_datetime(score_date) - timedelta(days = 1),pd.to_datetime(score_date) + timedelta(days = 3)):
+        temp = pd.read_csv("./competition_data/data/official_aq_master/"+dir_name+"/date/"+p.strftime("%Y-%m-%d") + ".csv")
+        label_data = pd.concat([label_data,temp],axis=0)
+    label_data_stations = {}
+    for station in label_data['station_id'].unique():
+        temp_label_data = label_data.loc[label_data['station_id'] == station,['record_time',target]].set_index('record_time')
+        # 先頭行がnullだと怒られるからとりあえず
+        temp_label_data.iloc[0, :] = 0
+        # データに抜けがあるので、ガワを作る
+        # ただ、最初のほうがnanだと怒られるので、最初の方のnanがない時間帯から始める
+        temp = pd.DataFrame(index=pd.date_range(temp_label_data.index[0],(pd.to_datetime(score_date) + timedelta(days = 3)).strftime("%Y-%m-%d") + ' 23:00:00', freq='H'))
+        temp_label_data = temp_label_data.join(temp,how="outer")
+        label_data_stations[station] = temp_label_data
+        logger.warning(station)
+
+    # submit data
+    submit_data = pd.read_csv("./submit/static/submit_files/"+submit.username+"/"+submit.filename)
+    submit_data['stationId'] = submit_data['test_id'].str.split('#').apply(lambda x:x[0])
+    submit_data['hour_num'] = submit_data['test_id'].str.split('#').apply(lambda x:x[1]).astype(int)
+
+    base_datetime = pd.to_datetime(score_date) + timedelta(days = 1)
+    temp_submit = submit_data.loc[submit_data['score_date'] == score_date]
+    temp_submit['datetime'] = temp_submit['hour_num'].apply(lambda x:base_datetime + timedelta(hours = x))
+
+    submit_data_stations = {}
+    for station in label_data_stations.keys():
+        submit_data_stations[station] = temp_submit.loc[temp_submit['stationId']==station, ['datetime',target]].set_index('datetime')
+
+    charts = []
+    for station in sorted(label_data_stations.keys()):
+        temp_dict = {'station':station}
+        temp_dict['chart_data_series'] = []
+        temp_dict['chart_data_series'].append({
+            'name':'label',
+            'data':str(label_data_stations[station].values.tolist()).replace('nan','null'),
+            'start_dt':str(label_data_stations[station].index[0])
+            })
+        temp_dict['chart_data_series'].append({
+            'name':'submit ' + score_date,
+            'data':str(submit_data_stations[station].values.tolist()).replace('nan',''),
+            'start_dt':base_datetime
+            })
+        charts.append(temp_dict)
+
+    context = {
+        'charts': charts,
+        'submit_id': submit_id,
+        'submit_timestamp' : submit.submit_timestamp,
+        'submit_username' : submit.username,
+        'submit_filename' : submit.filename,
+        'city' : city,
+        'target' : target
+    }
+    return render(request, 'mysubmission/mysubmission_detail_chart.html', context)
